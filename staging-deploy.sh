5e8b061ae4ceb772687c91f50ee9c2626efa3f6f
Michael <michael@managedkaos.com>
2019-01-25 23:40:28 -0800

    refactor the versioning scheme

--- staging-deploy.sh
+++ staging-deploy.sh
@@ -1,3 +1,3 @@
 aws elasticbeanstalk update-environment --application-name target-app \
     --environment-name staging \
-    --version-label 2019-01-25-1548485379
+    --version-label $(date +%F)-$(git rev-parse HEAD)
--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 aws elasticbeanstalk create-application-version --application-name target-app \
-    --version-label $(date +%F-%s)  \
+    --version-label $(date +%F)-$(git rev-parse HEAD)  \
     --source-bundle S3Bucket="cakedisk-api",S3Key="deploy.zip" \
     --no-auto-create-application
