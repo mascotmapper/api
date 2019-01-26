dfd421b118d76d18c797af55e7949dbd706536a1
Michael <michael@managedkaos.com>
2019-01-26 11:21:50 -0800

    Use git to make the zip

--- upload-new-version.sh
+++ upload-new-version.sh
@@ -4,10 +4,12 @@ bucket=cakedisk-api
 tag=$(date +%F)-$(git rev-parse --short HEAD)
 bundle=bundle-${tag}.zip
 
-zip -r /tmp/${bundle} . -x ".git*"
+git archive HEAD -o /tmp/${bundle}
+
 aws s3 cp /tmp/${bundle} s3://${bucket}
+
 aws elasticbeanstalk create-application-version \
     --application-name ${application} \
     --version-label ${tag}  \
-    --source-bundle S3Bucket="cakedisk-api",S3Key="${bundle}" \
+    --source-bundle S3Bucket="${bucket}",S3Key="${bundle}" \
     --no-auto-create-application
