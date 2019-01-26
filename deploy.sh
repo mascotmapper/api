a58e4b733ce2880204a0982d82847b791e2794ca
Michael <michael@managedkaos.com>
2019-01-26 00:42:47 -0800

    Add logic to wait on deployment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 environment=${1}
 application=target-app
 tag=$(date +%F)-$(git rev-parse HEAD)
+
+function get_status() {
+    echo $(aws elasticbeanstalk describe-environment-health --environment-name ${environment} --attribute-names HealthStatus --query="HealthStatus")
+}
+
+status=$(get_status)
+
+if [ $status != '"Ok"' ];
+then
+    echo "Unexpected status: ${status}"
+    exit 1
+fi
 
 aws elasticbeanstalk update-environment \
     --application-name ${application} \
     --environment-name ${environment} \
     --version-label ${tag}
+
+while [ $status == '"Ok"' ];
+do
+    status=$(get_status);
+    echo $status
+    sleep 10;
+done
+
+until [ $status == '"Ok"' ];
+do
+    status=$(get_status);
+    echo $status
+    sleep 10;
+done
